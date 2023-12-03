ae524c261eb4c6f18ff350fb227736178fc40603
Erwin Antepuesto <matcha.ccy@gmail.com>
2023-12-03 18:30:29 +0800

    belle | furbished navbar pt 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { VscMenu } from "react-icons/vsc";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoPersonCircle } from "react-icons/io5";
 
 const Navbar = () => {
-  const name = 'User';//FRONT END MAKE ME DYNAMIC
-  const email = 'admin@usc.edu.ph'//FRONT END MAKE ME DYNAMIC
+  const pageTitle = 'Dashboard';//BACK END MAKE ME DYNAMIC
+  const name = 'User';//BACK END MAKE ME DYNAMIC
+  const email = 'admin@usc.edu.ph'//BACK END MAKE ME DYNAMIC
 
   return (
     <div className='flex flex-row h-auto bg-fadedYellow items-center pl-8 p-3'>
       <div className='flex flex-row items-center justify-start w-2/4'>
-        <VscMenu size={50} color={"#448DB8"}  />
-        <div className="text-darkBlue text-5xl font-black ml-2 pl-3">Dashboard</div>
+        <div className="text-darkBlue text-5xl font-black">{pageTitle}</div>
       </div>
 
       <div className='flex flex-row items-center justify-end w-2/4 pr-8 gap-5'>
